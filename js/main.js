42b1f97333b4cd509ad1e0f591aad6bc0aa436fa
Ralf Prezia <ralfprezia@gmail.com>
2021-04-03 19:44:15 -0300

    primeiros passos

---
+++ js/main.js
@@ -0,0 +1,140 @@
+
+//DATA ATUAL
+var d = new Date();
+alert(d);//método que capta e envia a data completa e correta do dia do Usuário
+//alert(d.getDate());
+//alert(d.getDay());
+//alert(d.getFullYear());
+//alert(d.getHours());
+//alert(d.getMilliseconds());
+//alert(d.getMinutes());
+//alert(d.getMonth());
+//alert(d.getSeconds());
+//alert(d.getTime);
+//alert(d.getTimezoneOffset());
+//alert(d.getUTCDate());
+//alert(d.getUTCDay());
+//alert(d.getUTCFullYear());
+//alert(d.getUTCHours());
+//alert(d.getUTCMilliseconds());
+//alert(d.getUTCMinutes());
+//alert(d.getUTCMonth());
+//alert(d.getUTCSeconds());
+
+
+
+
+
+/*
+Looping For
+var i;
+for (i = 0; i < 5; i++){
+    console.log(i);
+    alert(i);
+};
+*/
+
+/*
+//Looping While
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    count++
+};
+*/
+
+/*//CONDICIONAL
+//recebendo um dado do usuário e atribuindo na variável idade
+var idade = prompt("Qual a sua idade?");//comando PROMPT == Console.WriteLine
+if (idade >= 18){
+    alert("maior idade");
+}
+else{
+    alert("menor idade");
+};
+*/
+
+/*
+//Lista de dicionário
+
+var frutas = [{ nome: "maçã", cor: "vermelha"},
+ { nome: "pêra", cor: "amarelo clarinho"},
+  { nome: "laranja", cor: "amarelo"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+
+
+/*
+//DICIONÁRIO
+var fruta = { nome: "maçã", cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+alert(fruta.nome);
+*/
+
+
+
+
+
+
+/* ARRAY
+
+var lista = ["maça", "pêra", "laranja"]
+
+console.log(lista);
+console.log(lista.toString);
+console.log(lista.join(" - "));//imprime a string separada por um traço (-).
+console.log(lista.join(" | "));//imprime a string separada por um traço pipeline (|).
+console.log(lista.join("  "));//imprime a string separada por um espaço ( ).
+
+//console.log(lista[0]);//maçã = 1° elemento do array.
+
+//console.log(lista.toString()[0]);//a = 1? elemento da string "maçã".
+
+//lista.push("uva");//método que adiciona uva na lista.
+
+//lista.pop("maçã");//método que remove a maçã da lista.
+
+//console.log(lista.reverse());//método que inverte a ordem na lista.
+
+//console.log(lista.length);//passa o tamanho da lista.
+
+//console.log(lista);//lista em formato de Array
+
+//console.log(lista.toString());//lista em formato string ex = maçã,pêra,laranja,uva.
+
+*/
+
+
+
+/* //STRINGS E INTEIROS
+var nome = "Ralf Prezia";
+var idade = 37;
+alert(nome + " tem " + idade + " anos.");
+console.log(nome);
+console.log(idade);
+console.log(nome + " tem " + idade + " anos.") 
+var idade2 = 10;
+console.log(idade + idade2);//SOMA DAS IDADES
+*/
+
+/*
+var idade = "37";//STRING
+var idade2 = "10";//STRING
+console.log(idade + idade2);//CONCATENARÁ AS DUAS STRINGS = 3710.
+alert(idade + idade2);
+ 
+
+var frase = "Japão é o melhor time do mundo";
+console.log(frase);
+console.log("Aqui usamos o replace onde trocamos o sujeito da frase:\n "
++ frase.replace("Japão", "Brasil"));//TROCA UMA PALAVRA POR OUTRA O CONSOLE
+//alert(frase.replace("Japão", "Brasil"));//REALIZA A TROCA NA MSG DE ALERTA
+console.log(frase.toUpperCase());//FRASE TODA EM MAIÚSCULA.
+console.log(frase.toLowerCase());//frase toda em minúsculo.
+*/
